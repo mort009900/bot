aa2759d2a573befacd5ca7099181e87946ceb444
mort009900 <mnm86940@gmail.com>
2025-04-20 21:22:45 +0300

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -5,10 +5,10 @@ app = Flask('')
 
 @app.route('/')
 def home():
-    return "تم تشغيل السيرفر بنجاح!"
+    return "✅ البوت يعمل الآن (keep_alive)"
 
 def run():
-    app.run(host="0.0.0.0", port=80)
+    app.run(host='0.0.0.0', port=8080)
 
 def keep_alive():
     t = Thread(target=run)
